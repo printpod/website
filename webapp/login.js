0b56689692a4c6a5706ddbf02a1ae7efcf4e444a
printpod <31548782+printpod@users.noreply.github.com>
2018-05-19 15:46:56 +1000

    -

--- webapp/login.js
+++ webapp/login.js
@@ -35,14 +35,15 @@ firebase.auth().onAuthStateChanged(function(user) {
 // Login function
 function login() {
 
-      var email = document.getElementById('emailAddress').value;
-      var password = document.getElementById('password').value;
-      firebase.auth.signInWithEmailAndPassword(email, password).catch(function(error){
+      var email = document.getElementById('emailAddress2').value;
+      var password = document.getElementById('password2').value;
+      firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error){
         var errorCode = error.code
         var errorMessage = error.message
         window.alert("Error: " + errorCode + errorMessage)
 
       });
+      document.getElementById("window").innerHTML = "Successfully signed in to PrintPod! Upload your documents!";
 }
   
 
@@ -53,18 +54,21 @@ function register(){
   var email = document.getElementById('emailAddress').value;
   var password = document.getElementById('password').value;
 
+  //Sign in
   firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+
   // Handle Errors here.
-  document.getElementById("window").innerHTML = "Welcome to PrintPod! Upload your documents!";
     var errorCode = error.code;
     var errorMessage = error.message;
     window.alert("Error: " + errorCode + "message :" + errorMessage)
-  // ...
 });
-   
+  // Replace div content after successful registration
+  document.getElementById("window").innerHTML = "Welcome to PrintPod! Upload your documents!";
+
 };
 
 document.getElementById('register-btn').addEventListener('click', register);
+document.getElementById('login-btn').addEventListener('click', login);
 
 
 
